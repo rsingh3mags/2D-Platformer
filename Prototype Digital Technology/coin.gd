1fb7274077b315a21f1aa5bd52006d424cb34087
rsingh3mags <135671947+rsingh3mags@users.noreply.github.com>
2023-10-18 09:40:08 +1300

    made level 3
    made level 3

--- Prototype Digital Technology/coin.gd
+++ Prototype Digital Technology/coin.gd
@@ -3,15 +3,17 @@ extends Area2D
 signal coin_collected
 
 
-
+# Animation for the coin
 func _ready():
 	$AnimationPlayer.play("idle")
 
+# Function for when the player enters the coin and signals the HUD.
+# 
 func _on_body_entered(body):
 	if body.name == "Player":
 		Global.coins_collected += 1
 		coin_collected.emit()
-		$"Gem _Collected".play()
+		$"Gem _Collected".play() 
 		hide()
 
  
--- Prototype Digital Technology/player.gd
+++ Prototype Digital Technology/player.gd
@@ -18,11 +18,14 @@ func _physics_process(delta):
 	var in_door = get_parent().find_child("Door").door
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if Input.is_action_just_pressed("jump") and is_on_floor() and gravity and not in_door and not in_Hitbox:
+	if (
+			Input.is_action_just_pressed("jump") and is_on_floor()
+			and gravity and not in_door and not in_Hitbox
+	):
 		velocity.y = JUMP_VELOCITY
 		$JumpSFX.play()
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement.
+	# 
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction and not in_door and not in_Hitbox:
 		velocity.x = direction * SPEED
@@ -34,6 +37,7 @@ func _physics_process(delta):
 	elif not in_Hitbox:
 		$AnimatedSprite2D.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, float(SPEED) / 2)
+		
 	if dying == true:
 		$AnimatedSprite2D.play("death")
 		await get_tree().create_timer(0.5).timeout
@@ -43,6 +47,7 @@ func _physics_process(delta):
 		$AnimatedSprite2D.play("jump")
 
 	
+# When Player enters the Hitbox
 	move_and_slide()
 func _on_area_2d_area_entered(area):
 	if area.has_meta("hitbox"):
